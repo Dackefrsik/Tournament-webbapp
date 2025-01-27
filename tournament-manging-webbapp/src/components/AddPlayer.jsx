e961d3c57c6042d289a042a095c73b61cbf94630
Dackefrsik <danielfrisk21@gmail.com>
2025-01-27 22:05:38 +0100

    Funktionelitet för att ange ett namn
    Nu kollar webbappen om textruttan är tom och i så fall sätter den ut en röd ram på den. När användaren sedan fokuserar på den igen så försvinner den. Skulle rutan där emot ha text i sig så kommer den att försvinna. Senare ska även namnet sparas

--- tournament-manging-webbapp/src/components/AddPlayer.jsx
+++ tournament-manging-webbapp/src/components/AddPlayer.jsx
@@ -6,6 +6,13 @@ function AddPlayer({tornament}){
     //Vektor för att hålla i element för att lägga
     //till en spelare med
     let Elemets =[];
+    
+    //Funktion som kollar vilken kanpp i formuläret
+    //Tryckts på för att sedan ta bort den
+    function handleVisibility(i){
+        const div = document.querySelectorAll(".addPlayer");
+        div[i].style.display = "none"; 
+    }
 
     //Loop för att bygga ett "kort" för varje 
     //delatagare som ska läggas till
@@ -13,13 +20,13 @@ function AddPlayer({tornament}){
         
         //Puschar det till vektorn Elements 
         Elemets.push(
-            <div className="border p-2 m-1 rounded-3">
+            <div className="addPlayer border p-2 m-1 rounded-3">
                 <h3 className="mt-2 ms-2">
                     Player {i + 1}
                 </h3>
                 {/*Kallar på komponenten som visar ett 
                 formulär för att lägga till delatagare men namn*/}
-                <AddPlayerForm/>
+                <AddPlayerForm handleVisibility={handleVisibility} buttonID={i}/>
             </div>
         )
     }
--- tournament-manging-webbapp/src/components/AddPlayerForm.jsx
+++ tournament-manging-webbapp/src/components/AddPlayerForm.jsx
@@ -1,20 +1,45 @@
 
+import React, { useRef } from "react";
+/*Funktion som tar emot funktion för att ändra synlighet 
+och id vär at veta vilken div vars synlighet ska ändras*/
+function AddPlayerForm({handleVisibility, buttonID}){
+    
+    //Sätter en referens på ett element istället 
+    // för att använda quewrySelect
+    const inputNameRef = useRef(null);
 
-function AddPlayerForm(){
+    //Funktion som hanterar klickevent på knappen för att
+    //lägga till spelare
+    const handleClick = (event) => {
+        event.preventDefault();
 
+        //Tar bort div:n med formuläret i om det har text i sig
+        if(inputNameRef.current && inputNameRef.current.value.trim() !== ""){
+            handleVisibility(buttonID);
+        }
+        else{
+            //Ger input rutan en röd ram om den är tom
+            inputNameRef.current.style.border = "3px solid red";
+        }
+    }
+
+    //Funktion som tar bort den röda ramen på input rutan
+    function inputFocus(){
+        inputNameRef.current.style.border = "";
+    }
 
     return(
-        <form action="" className="form-group">
+        <form action="" className="form-group" >
             <div className="d-flex flex-column">
                 <div className="row">
                     <div className="col-9">
                         <div className="form-floating">
-                            <input type="text" name="Name" className="mb-2 form-control" id="Name" placeholder="Namm"/>
+                            <input type="text" name="Name" className="mb-2 form-control" id="Name" placeholder="Namm" ref={inputNameRef} onFocus={() => inputFocus()}/>
                             <label htmlFor="Name">Namn</label>
                         </div>
                     </div>
                     <div className="col-3 d-flex">
-                        <input type="submit" value="Add" className="mb-2 form-control btn btn-success btn-lg p-2" />
+                        <input type="submit" value="Add" className="mb-2 form-control btn btn-success btn-lg p-2" onClick={handleClick}/>
                     </div>
                 </div>
             </div>
